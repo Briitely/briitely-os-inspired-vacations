98f0b1083df1283835f640d2a1ef00490370e2f3
Lana Walsh <info@briitely.com>
2026-09-09 15:22:04 -0600

    Allow general tasks outside Travel Files

---
+++ supabase/migrations/20260909154500_allow_general_tasks.sql
@@ -0,0 +1,9 @@
+alter table public.travel_file_tasks
+  alter column travel_file_id drop not null;
+
+alter table public.travel_file_tasks
+  add column if not exists task_context text not null default 'travel_file'
+  check (task_context in ('travel_file','general'));
+
+update public.travel_file_tasks
+set task_context = case when travel_file_id is null then 'general' else 'travel_file' end;
